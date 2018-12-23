a948c2a8d9ba44a5cd7c6d230912916fbe3cdb1a
andreluiz1035 <andreluiz.mcse@gmail.com>
2018-12-23 21:04:43 -0200

    10 commit

---
+++ query.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+#Verify devices with status open (no action executed yet)
+
+#Send query comand to these devices and change status to waiting_conn
+
+
+
+
+
+#Verify devices with status waiting_conn  and send unenrol command
+
+#If the device is waiting_conn for more than 2 hours, send a message to the client to connect device
+
+#If the device is wating_conn for more than 2 days, send a message to support
+
+
+
+#Change status to waiting_unenroll.
+
+
+
+#Verify devices with status waiting_unenroll, if possible change to unenrolled.
+
+
+
+#Verify devices with status unenrolled and send message to the client
+
+
+
+
